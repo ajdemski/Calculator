399ca14b7853c1ea6b5ce40a26fc44704aec7649
Aaron Demski <ajdemski@frontier.com>
2023-01-09 13:56:03 -0800

    script-add

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-function saySomething(whatToSay) {
-    window.alert(whatToSay);
-  }
-  
-  function add(number1, number2) {
+// business logic
+function add(number1, number2) {
     return number1 + number2;
   }
   
-  const result = add(3, 5);
-  const outputText = "The sum is " + result + ".";    // this line is new
-  saySomething(outputText);                           // this line is new
+  // user interface logic 
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  
+  window.alert(add(number1, number2));
