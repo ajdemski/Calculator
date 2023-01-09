63a1d64d2894f91a0ad86e7d8d4517a19f7b7c7d
Aaron Demski <ajdemski@frontier.com>
2023-01-09 15:55:17 -0800

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
-// business logic
-function add(number1, number2) {
-    return number1 + number2;
-  }
-  
-  // user interface logic 
-  const number1 = prompt("Enter a number:");
-  const number2 = prompt("Enter another number:");
-  
-  window.alert(add(number1, number2));
+function calcSum(){
+    let box1 = document.getElementById("box1").value;
+    let box2 = document.getElementById("box2").value;
+    let sum = Number(box1) + Number(box2);
+    document.getElementById("+").value = sum;
+    }
+
+function calcSum1(){
+    let box1 = document.getElementById("box3").value;
+    let box2 = document.getElementById("box4").value;
+    let sum1 = Number(box3) + Number(box4);
+    document.getElementById("+").value = sum1;
+    }
