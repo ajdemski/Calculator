377f7eaf0f6692f25ae22e36abcbb1cd8efd3895
Aaron Demski <ajdemski@frontier.com>
2023-01-09 13:34:26 -0800

    wip-script

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function saySomething(whatToSay) {
+    window.alert(whatToSay);
+  }
+  
+  function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  const result = add(3, 5);
+  const outputText = "The sum is " + result + ".";    // this line is new
+  saySomething(outputText);                           // this line is new
